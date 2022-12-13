6e2fcbc9b5486508ada45cc6e74dc1c5e76b5672
Kristina Kazakova <kris.m98@icloud.com>
2022-12-13 10:28:02 +0700

    1editing

--- hw4.py
+++ hw4.py
@@ -2,15 +2,15 @@ def funcs(fullname, *args):
     fullname = fullname.__name__.replace("_", " ").upper() + ":"
     print(fullname, *args)
 
-    def open_browser(browser_name):
-        funcs(open_browser, browser_name)
+def open_browser(browser_name):
+    funcs(open_browser, browser_name)
 
-        def go_to_companyname_homepage(page_url):
-            funcs(go_to_companyname_homepage, page_url)
+def go_to_companyname_homepage(page_url):
+    funcs(go_to_companyname_homepage, page_url)
 
-        def find_registration_button_on_login_page(page_url, button_text):
-            funcs(find_registration_button_on_login_page, page_url, button_text)
+def find_registration_button_on_login_page(page_url, button_text):
+    funcs(find_registration_button_on_login_page, page_url, button_text)
 
-        open_browser("Chrome")
-        go_to_companyname_homepage("https:/google.com")
-        find_registration_button_on_login_page("URL", "Login")
+open_browser("Chrome")
+go_to_companyname_homepage("https:/google.com")
+find_registration_button_on_login_page("URL", "Login")
